bc4c97c93a9afca4709da0be4ec922eee3242902
Robert Jordan <jrob800@gmail.com>
2021-06-03 10:33:34 -0400

    Update mj.util modules descriptions
    * List modules and functionalities.

--- src/mj/util/__init__.py
+++ src/mj/util/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 """Various utilities and helper functions submodule.
+
+modules:
+  mj.util.color    - terminal ansi color helpers and escapes.
+  mj.util.typecast - type casting for str and bytes ('cp932'), float, and fixed-width ints.
 """
 
 __version__ = '0.1.0'
-__date__    = '2021-05-07'
+__date__    = '2021-06-03'
 __author__  = 'Robert Jordan'
 
 #######################################################################################

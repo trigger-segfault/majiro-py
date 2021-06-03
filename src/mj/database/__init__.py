8e3629c9b1d09049ac5b52809887a437912b02e5
Robert Jordan <jrob800@gmail.com>
2021-06-03 10:27:02 -0400

    Update modules descriptions in mj.database
    * Add new modules and submodules to list

--- src/mj/database/__init__.py
+++ src/mj/database/__init__.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-"""Majiro information database module
+"""Majiro information database and analysis module.
+
+modules:
+  mj.database.usage     - usage analysis of hashes and instructions in scripts. (may move to `mj.project`)
 
 submodules:
-  database.hashes - collection of known hashes and hash names
+  mj.database.hashes    - collection of known hashes and hash names.
+  mj.database.sheets    - reading and writing "Majiro Data" Google Sheets information.
+  mj.database.unhashers - various tools and methods for solving hashed names.
+
 """
 
 __version__ = '0.1.0'
-__date__    = '2021-06-02'
+__date__    = '2021-06-03'
 __author__  = 'Robert Jordan'
 
 #######################################################################################

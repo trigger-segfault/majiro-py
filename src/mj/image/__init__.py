fc3187f10d69f43b1b7c175cb99920b0d207fb47
Robert Jordan <jrob800@gmail.com>
2021-06-03 10:52:40 -0400

    Add mj.image modules descriptions

---
+++ src/mj/image/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+"""Majiro image file reading and writing tools.
+
+modules:
+  mj.image.rcimage - classes for reading and writing `.rct` and `.rc8` image pixel data.
+
+"""
+
+__version__ = '1.0.0'
+__date__    = '2021-06-03'
+__author__  = 'Robert Jordan'
+
+#__all__ = []
+
+#######################################################################################
+
+

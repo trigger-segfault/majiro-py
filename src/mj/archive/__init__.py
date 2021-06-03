54903d56abe9ae2b30dd51ac4d564f6f26d6b270
Robert Jordan <jrob800@gmail.com>
2021-06-03 10:47:52 -0400

    Add mj.archive modules descriptions

---
+++ src/mj/archive/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+"""Majiro archive file reading and writing tools.
+
+modules:
+  mj.archive.arcextfile - stream to access file entries directly from `.arc` files.
+  mj.archive.arcfile    - class for reading file entries from `.arc` archive files.
+
+"""
+
+__version__ = '1.0.0'
+__date__    = '2021-06-03'
+__author__  = 'Robert Jordan'
+
+#__all__ = []
+
+#######################################################################################
+
+

12119bd1126c281c4638cda6d663a947ae1eb7d6
Robert Jordan <jrob800@gmail.com>
2021-06-03 10:42:33 -0400

    Update mj.script modules descriptions
    * Change submodule description as it's no longer the root package module.
    * List all "usable" modules and submodules.

--- src/mj/script/__init__.py
+++ src/mj/script/__init__.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-"""Majiro VN engine Python library package
+"""Majiro script file reading and writing tools.
 
 modules:
-  mj.crypt - cryptographic and hashing functions.
-  mj.name  - identifier name text parsing. (similar in function to `os.path`)
-  mj.identifier  - identifier name and hash handling.
+  mj.script.disassembler - export scripts to MjIL intermediate language.
+  mj.script.flags        - script enum types and bitmasks.
+  mj.script.instruction  - script bytecode instruction class.
+  mj.script.mjoscript    - class for reading and writing binary `.mjo` script files.
+  mj.script.opcodes      - collection of all instruction opcodes.
 
 submodules:
-  mj.database    - database of useful Majiro engine information (mainly known hash value names).
-  mj.script      - tools and classes for reading and writing `.mjo` script files.
+  mj.script.analysis     - analysis of single-script control flow and functions.
 
 """
 
 __version__ = '1.0.0'
-__date__    = '2021-05-04'
+__date__    = '2021-06-03'
 __author__  = 'Robert Jordan'
 
 __all__ = ['MjoType', 'MjoScope', 'MjoModifier', 'MjoInvert', 'MjoDimension', 'MjoFlags', 'Opcode', 'Instruction', 'FunctionIndexEntry', 'MjoScript']

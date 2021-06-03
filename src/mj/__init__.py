55b7897d43d577f19ff88664f4a5c6b4c4e2727c
Robert Jordan <jrob800@gmail.com>
2021-06-03 10:17:56 -0400

    Update modules descriptions in package root
    * Add remaining unlisted modules and sub modules.
    * Update some descriptions, as functionality has expanded.

--- src/mj/__init__.py
+++ src/mj/__init__.py
@@ -3,18 +3,23 @@
 """Majiro VN engine Python library package
 
 modules:
-  mj.crypt - cryptographic and hashing functions.
-  mj.name  - identifier name text parsing. (similar in function to `os.path`)
+  mj.crypt       - cryptographic, hashing, and unhashing functions.
+  mj.name        - identifier name text parsing and constants. (similar to `os.path` module)
   mj.identifier  - identifier name and hash handling.
+  mj.signature   - signature representation classes for variables, functions, etc.
 
 submodules:
-  mj.database    - database of useful Majiro engine information (mainly known hash value names).
-  mj.script      - tools and classes for reading and writing `.mjo` script files.
+  mj.archive     - reading file entries from `.arc` archive files.
+  mj.database    - useful Majiro engine information and extra analysis tools. (mainly unhashed names)
+  mj.image       - reading and writing `.rct` and `.rc8` image pixel data.
+  mj.project     - gathering and organizing codebase information for a game's scripts.
+  mj.script      - reading and writing `.mjo` script files.
+  mj.util        - helper functions used throughout the package.
 
 """
 
-__version__ = '1.0.0'
-__date__    = '2021-05-04'
+__version__ = '1.1.0'
+__date__    = '2021-06-03'
 __author__  = 'Robert Jordan'
 
 # __all__ = ['LOCAL_VARS', 'LOCAL_VARS_LOOKUP', 'THREAD_VARS', 'THREAD_VARS_LOOKUP', 'SAVEFILE_VARS', 'SAVEFILE_VARS_LOOKUP', 'PERSISTENT_VARS', 'PERSISTENT_VARS_LOOKUP', 'FUNCTIONS', 'FUNCTIONS_LOOKUP', 'SYSCALLS', 'SYSCALLS_LOOKUP', 'GROUPS', 'GROUPS_LOOKUP', 'CALLBACKS', 'CALLBACKS_LOOKUP', 'SYSCALLS_LIST', 'VARIABLES', 'VARIABLES_LOOKUP']
